cb34a8c38e76394e35d23b82e0ca39c582be28c5
knight <karvin_jimenez@hotmail.com>
2024-12-29 20:32:11 -0400

    project started.

--- source/main.cpp
+++ source/main.cpp
@@ -1,4 +1,6 @@
 #include <citro2d.h>
+#include <iostream>
+#include <vector>
 
 // the 3ds has different screen width, but the same screen height.
 const int TOP_SCREEN_WIDTH = 400;
@@ -19,8 +21,6 @@ C2D_Text staticTexts[1];
 
 float textSize = 1.0f;
 
-C2D_Image sprite;
-
 // Create colors
 const u32 WHITE = C2D_Color32(0xFF, 0xFF, 0xFF, 0xFF);
 const u32 BLACK = C2D_Color32(0x00, 0x00, 0x00, 0x00);
@@ -38,15 +38,164 @@ typedef struct
 	unsigned int color;
 } Rectangle;
 
-Rectangle spriteBounds = {TOP_SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 0, 32, 32, WHITE};
-Rectangle ball = {TOP_SCREEN_WIDTH / 2 - 50, SCREEN_HEIGHT / 2, 0, 20, 20, WHITE};
-Rectangle bottomBounds = {BOTTOM_SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 0, 32, 32, BLUE};
-Rectangle touchBounds = {0, 0, 0, 8, 8, WHITE};
+typedef struct
+{
+	C2D_Image texture;
+	Rectangle textureBounds;
+} Sprite;
 
-const int PLAYER_SPEED = 10;
+Sprite shipSprite;
+Sprite playerSprite;
+Sprite alienSprite1;
+Sprite alienSprite2;
+Sprite alienSprite3;
+Sprite structureSprite;
 
-int ballVelocityX = 5;
-int ballVelocityY = 5;
+typedef struct
+{
+	Rectangle bounds;
+	bool isDestroyed;
+} Laser;
+
+std::vector<Laser> playerLasers;
+std::vector<Laser> alienLasers;
+
+float lastTimePlayerShoot;
+float lastTimeAliensShoot;
+
+typedef struct
+{
+	Sprite sprite;
+	int lives;
+	int speed;
+	int score;
+} Player;
+
+Player player;
+
+typedef struct
+{
+	float x;
+	Sprite sprite;
+	int points;
+	int velocityX;
+	bool shouldMove;
+	bool isDestroyed;
+} MysteryShip;
+
+MysteryShip mysteryShip;
+
+float lastTimeMysteryShipSpawn;
+
+typedef struct
+{
+	Sprite sprite;
+	int lives;
+	bool isDestroyed;
+} Structure;
+
+std::vector<Structure> structures;
+
+typedef struct
+{
+	float x;
+	Sprite sprite;
+	int points;
+	int velocity;
+	bool isDestroyed;
+} Alien;
+
+std::vector<Alien> aliens;
+
+bool shouldChangeVelocity = false;
+
+C2D_SpriteSheet sheet;
+
+std::vector<Alien> createAliens()
+{
+	C2D_Image sprite = C2D_SpriteSheetGetImage(sheet, 0);
+
+	Rectangle initialBounds = {0, 0, 0, 16, 16, WHITE};
+
+	alienSprite1 = {sprite, initialBounds};
+	alienSprite2 = {sprite, initialBounds};
+	alienSprite3 = {sprite, initialBounds};
+
+	std::vector<Alien> aliens;
+
+	// 5*11 Aliens
+	aliens.reserve(55);
+
+	int positionX;
+	int positionY = 25;
+	int alienPoints = 8;
+
+	Sprite actualSprite;
+
+	for (int row = 0; row < 5; row++)
+	{
+		positionX = 75;
+
+		switch (row)
+		{
+		case 0:
+			actualSprite = alienSprite3;
+			break;
+
+		case 1:
+		case 2:
+			actualSprite = alienSprite2;
+			break;
+
+		default:
+			actualSprite = alienSprite1;
+		}
+
+		for (int columns = 0; columns < 11; columns++)
+		{
+			actualSprite.textureBounds.x = positionX;
+			actualSprite.textureBounds.y = positionY;
+
+			Alien actualAlien = {(float)positionX, actualSprite, alienPoints, 50, false};
+
+			aliens.push_back(actualAlien);
+			positionX += 30;
+		}
+
+		alienPoints--;
+		positionY += 25;
+	}
+
+	return aliens;
+}
+
+void aliensMovement(float deltaTime)
+{
+	for (Alien &alien : aliens)
+	{
+		alien.x += alien.velocity * deltaTime;
+		alien.sprite.textureBounds.x = alien.x;
+
+		float alienPosition = alien.sprite.textureBounds.x + alien.sprite.textureBounds.w;
+
+		if ((!shouldChangeVelocity && alienPosition > TOP_SCREEN_WIDTH) || alienPosition < alien.sprite.textureBounds.w)
+		{
+			shouldChangeVelocity = true;
+			break;
+		}
+	}
+
+	if (shouldChangeVelocity)
+	{
+		for (Alien &alien : aliens)
+		{
+			alien.velocity *= -1;
+			alien.sprite.textureBounds.y += 5;
+		}
+
+		shouldChangeVelocity = false;
+	}
+}
 
 bool hasCollision(Rectangle &bounds, Rectangle &ball)
 {
@@ -54,57 +203,211 @@ bool hasCollision(Rectangle &bounds, Rectangle &ball)
 		   bounds.y < ball.y + ball.h && bounds.y + bounds.h > ball.y;
 }
 
+void checkCollisionBetweenStructureAndLaser(Laser &laser)
+{
+	for (Structure &structure : structures)
+	{
+		if (!structure.isDestroyed && hasCollision(structure.sprite.textureBounds, laser.bounds))
+		{
+			laser.isDestroyed = true;
+
+			structure.lives--;
+
+			if (structure.lives == 0)
+			{
+				structure.isDestroyed = true;
+			}
+
+			// Mix_PlayChannel(-1, explosionSound, 0);
+
+			break;
+		}
+	}
+}
+
+void removeDestroyedElements()
+{
+	for (auto iterator = aliens.begin(); iterator != aliens.end();)
+	{
+		if (iterator->isDestroyed)
+		{
+			aliens.erase(iterator);
+		}
+		else
+		{
+			iterator++;
+		}
+	}
+
+	for (auto iterator = playerLasers.begin(); iterator != playerLasers.end();)
+	{
+		if (iterator->isDestroyed)
+		{
+			playerLasers.erase(iterator);
+		}
+		else
+		{
+			iterator++;
+		}
+	}
+
+	for (auto iterator = alienLasers.begin(); iterator != alienLasers.end();)
+	{
+		if (iterator->isDestroyed)
+		{
+			alienLasers.erase(iterator);
+		}
+		else
+		{
+			iterator++;
+		}
+	}
+}
+
 void update()
 {
-	// Respond to user input
 	int keyHeld = hidKeysHeld();
 
-	if (keyHeld & KEY_LEFT && spriteBounds.x > 0)
+	if (keyHeld & KEY_LEFT && player.sprite.textureBounds.x > 0)
 	{
-		spriteBounds.x -= PLAYER_SPEED;
+		player.sprite.textureBounds.x -= player.speed /* deltaTime*/;
 	}
 
-	else if (keyHeld & KEY_RIGHT && spriteBounds.x < TOP_SCREEN_WIDTH - spriteBounds.w)
+	else if (keyHeld & KEY_RIGHT && player.sprite.textureBounds.x < TOP_SCREEN_WIDTH - player.sprite.textureBounds.w)
 	{
-		spriteBounds.x += PLAYER_SPEED;
+		player.sprite.textureBounds.x += player.speed /* deltaTime*/;
 	}
 
-	else if (keyHeld & KEY_UP && spriteBounds.y > 0)
+	if (!mysteryShip.shouldMove)
 	{
-		spriteBounds.y -= PLAYER_SPEED;
-	}
-	
-	else if (keyHeld & KEY_DOWN && spriteBounds.y < SCREEN_HEIGHT - spriteBounds.h)
-	{
-		spriteBounds.y += PLAYER_SPEED;
-	}
+		// lastTimeMysteryShipSpawn += deltaTime;
 
-	if (ball.x < 0 || ball.x > TOP_SCREEN_WIDTH - ball.w)
-	{
-		ballVelocityX *= -1;
+		if (lastTimeMysteryShipSpawn >= 10)
+		{
+			lastTimeMysteryShipSpawn = 0;
 
-		ball.color = GREEN;
+			mysteryShip.shouldMove = true;
+		}
 	}
 
-	else if (ball.y < 0 || ball.y > SCREEN_HEIGHT - ball.h)
+	if (mysteryShip.shouldMove)
 	{
-		ballVelocityY *= -1;
+		if (mysteryShip.sprite.textureBounds.x > TOP_SCREEN_WIDTH + mysteryShip.sprite.textureBounds.w || mysteryShip.sprite.textureBounds.x < -80)
+		{
+			mysteryShip.velocityX *= -1;
+			mysteryShip.shouldMove = false;
+		}
 
-		ball.color = RED;
+		// mysteryShip.x += mysteryShip.velocityX * deltaTime;
+		mysteryShip.sprite.textureBounds.x = mysteryShip.x;
 	}
 
-	else if (hasCollision(spriteBounds, ball))
+	// if (SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_A))
+	// {
+	//     lastTimePlayerShoot += deltaTime;
+
+	//     if (lastTimePlayerShoot >= 0.35)
+	//     {
+	//         SDL_Rect laserBounds = {player.sprite.textureBounds.x + 10, player.sprite.textureBounds.y - player.sprite.textureBounds.h, 2, 8};
+
+	//         playerLasers.push_back({laserBounds, false});
+
+	//         lastTimePlayerShoot = 0;
+
+	//         Mix_PlayChannel(-1, laserSound, 0);
+	//     }
+	// }
+
+	for (Laser &laser : playerLasers)
 	{
-		ballVelocityX *= -1;
-		ballVelocityY *= -1;
+		// laser.bounds.y -= 200 * deltaTime;
 
-		ball.color = BLUE;
+		if (laser.bounds.y < 0)
+			laser.isDestroyed = true;
 
-		collisionCounter++;
+		if (!mysteryShip.isDestroyed && hasCollision(mysteryShip.sprite.textureBounds, laser.bounds))
+		{
+			laser.isDestroyed = true;
+			mysteryShip.isDestroyed = true;
+
+			player.score += mysteryShip.points;
+
+			// std::string scoreString = "score: " + std::to_string(player.score);
+
+			// updateTextureText(scoreTexture, scoreString.c_str(), fontSquare, renderer);
+
+			// Mix_PlayChannel(-1, explosionSound, 0);
+
+			break;
+		}
+
+		for (Alien &alien : aliens)
+		{
+			if (!alien.isDestroyed && hasCollision(alien.sprite.textureBounds, laser.bounds))
+			{
+				alien.isDestroyed = true;
+				laser.isDestroyed = true;
+
+				player.score += alien.points;
+
+				// std::string scoreString = "score: " + std::to_string(player.score);
+
+				// updateTextureText(scoreTexture, scoreString.c_str(), fontSquare, renderer);
+
+				// Mix_PlayChannel(-1, explosionSound, 0);
+
+				break;
+			}
+		}
+
+		checkCollisionBetweenStructureAndLaser(laser);
 	}
 
-	ball.x += ballVelocityX;
-	ball.y += ballVelocityY;
+	// lastTimeAliensShoot += deltaTime;
+
+	if (aliens.size() > 0 && lastTimeAliensShoot >= 0.6)
+	{
+		int randomAlienIndex = rand() % aliens.size();
+
+		Alien alienShooter = aliens[randomAlienIndex];
+
+		Rectangle laserBounds = {alienShooter.sprite.textureBounds.x + 10, alienShooter.sprite.textureBounds.y + alienShooter.sprite.textureBounds.h, 0, 2, 8, WHITE};
+
+		alienLasers.push_back({laserBounds, false});
+
+		lastTimeAliensShoot = 0;
+
+		// Mix_PlayChannel(-1, laserSound, 0);
+	}
+
+	for (Laser &laser : alienLasers)
+	{
+		// laser.bounds.y += 200 * deltaTime;
+
+		if (laser.bounds.y > SCREEN_HEIGHT)
+			laser.isDestroyed = true;
+
+		if (player.lives > 0 && hasCollision(player.sprite.textureBounds, laser.bounds))
+		{
+			laser.isDestroyed = true;
+
+			player.lives--;
+
+			// std::string liveString = "lives: " + std::to_string(player.lives);
+
+			// updateTextureText(liveTexture, liveString.c_str(), fontSquare, renderer);
+
+			// Mix_PlayChannel(-1, explosionSound, 0);
+
+			break;
+		}
+
+		checkCollisionBetweenStructureAndLaser(laser);
+	}
+
+	aliensMovement(1);
+
+	removeDestroyedElements();
 }
 
 void renderTopScreen()
@@ -113,25 +416,10 @@ void renderTopScreen()
 	C2D_TargetClear(topScreen, BLACK);
 	C2D_SceneBegin(topScreen);
 
-	// (float x, float y, float z, float  w, float h, u32 clr)
-	C2D_DrawRectSolid(ball.x, ball.y, ball.z, ball.w, ball.h, ball.color);
-
-	// ( 	C2D_Image img, float x, float y, float depth, const C2D_ImageTint *tint C2D_OPTIONALnullptr, float scaleX C2D_OPTIONAL1.0f, float scaleY C2D_OPTIONAL1.0f)
-	C2D_DrawImageAt(sprite, spriteBounds.x, spriteBounds.y, 0, NULL, 1, 1);
+	C2D_DrawImageAt(player.sprite.texture, player.sprite.textureBounds.x, player.sprite.textureBounds.y, 0, NULL, 1, 1);
 
 	if (isGamePaused)
 	{
-		// Draws text using the GPU.
-
-		// Parameters
-		//     [in]	text	Pointer to text object.
-		//     [in]	flags	Text drawing flags.
-		//     [in]	x	Horizontal position to draw the text on.
-		//     [in]	y	Vertical position to draw the text on. If C2D_AtBaseline is not specified (default), this is the top left corner of the block of text; otherwise this is the position of the baseline of the first line of text.
-		//     [in]	z	Depth value of the text. If unsure, pass 0.0f.
-		//     [in]	scaleX	Horizontal textSize of the font. 1.0f corresponds to the native textSize of the font.
-		//     [in]	scaleY	Vertical textSize of the font. 1.0f corresponds to the native textSize of the font.
-		// Draw static text strings
 		C2D_DrawText(&staticTexts[0], C2D_AtBaseline | C2D_WithColor, 110, 60, 0, textSize, textSize, WHITE);
 	}
 
@@ -144,7 +432,7 @@ void renderBottomScreen()
 	C2D_TargetClear(bottomScreen, BLACK);
 	C2D_SceneBegin(bottomScreen);
 
-	C2D_DrawRectSolid(bottomBounds.x, bottomBounds.y, bottomBounds.z, bottomBounds.w, bottomBounds.h, bottomBounds.color);
+	// C2D_DrawRectSolid(bottomBounds.x, bottomBounds.y, bottomBounds.z, bottomBounds.w, bottomBounds.h, bottomBounds.color);
 
 	C2D_TextBufClear(textDynamicBuffer);
 
@@ -183,9 +471,16 @@ int main(int argc, char *argv[])
 	// Optimize the static text strings
 	C2D_TextOptimize(&staticTexts[0]);
 
-	// Load the sprite
-	C2D_SpriteSheet sheet = C2D_SpriteSheetLoad("romfs:/gfx/alien_1.t3x");
-	sprite = C2D_SpriteSheetGetImage(sheet, 0);
+	sheet = C2D_SpriteSheetLoad("romfs:/gfx/alien_1.t3x");
+
+	// aliens = createAliens();
+
+	C2D_Image playerSprite = C2D_SpriteSheetGetImage(sheet, 0);
+
+	Rectangle initialBounds = {100, 200, 0, 20, 18, WHITE};
+
+	Sprite sprite = {playerSprite, initialBounds};
+	player = {sprite, 2, 10, 0};
 
 	touchPosition touch;
 
@@ -197,23 +492,8 @@ int main(int argc, char *argv[])
 		// Read the touch screen coordinates
 		hidTouchRead(&touch);
 
-		if (touch.px > 0 && touch.py > 0 && touch.px < BOTTOM_SCREEN_WIDTH - bottomBounds.w && touch.py < SCREEN_HEIGHT - bottomBounds.h)
-		{
-			bottomBounds.x = touch.px;
-			bottomBounds.y = touch.py;
-		}
-
-		touchBounds.x = touch.px;
-		touchBounds.y = touch.py;
-
-		if (hasCollision(touchBounds, bottomBounds))
-		{
-			bottomBounds.color = RED;
-		}
-		else
-		{
-			bottomBounds.color = BLUE;
-		}
+		// touch.px
+		// touch.py
 
 		int keyDown = hidKeysDown();
 
